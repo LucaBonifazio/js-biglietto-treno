89af9174ca2121fa114e377ededc752fb6da545a
LucaBonifazio <luca.bonifazio13@gmail.com>
2022-10-20 16:01:03 +0200

    working progress

--- js/script.js
+++ js/script.js
@@ -7,20 +7,19 @@
 
 
 const Km = parseFloat(prompt('Benvenuto su TrenItalia.com, quanti kilometri desidera fare?'));
-console.log(`I Km desiderati sono:${Km}`); 
+console.log(`I Km desiderati sono: ${Km} Km`); 
 const age = parseInt(prompt('Quanti anni ha?')); 
-console.log(`La sua età è:${age}`); 
-const price = parseFloat(Km * 0.21).toFixed(2); 
-console.log(`Il prezzo del biglietto è di:${price}€`); 
-let sales = "";
-const newPrice = (price - sales)
+console.log(`La sua età è: ${age} anni`); 
+const price = parseFloat(Km * 0.21); 
+console.log(`Il prezzo del biglietto è di: ${price} €`).toFixed(2); 
+let sales = (price * (''));
 
 if (age < 18) {
-    sales = ((price * 20) / 100).toFixed(2); 
-    console.log(`Essendo minorenne ha diritto al 20% di sconto, pertanto il prezzo sarà di:${newPrice}€`); 
+    sales = ((price * 20) / 100); 
+    console.log(`Essendo minorenne ha diritto al 20% di sconto, pertanto il prezzo sarà di: ${sales} €`).toFixed(2)
 } else if (age > 65) {
-    sales = ((price * 40) / 100).toFixed(2); 
-    console.log(`Essendo over 65 anni ha diritto al 40% di sconto, pertanto il prezzo sarà di:${newPrice}€`);
+    sales = ((price * 40) / 100); 
+    console.log(`Essendo over 65 anni ha diritto al 40% di sconto, pertanto il prezzo sarà di: ${sales} €`).toFixed(2); ;
 }
 
 
