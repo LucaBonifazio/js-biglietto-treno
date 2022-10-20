71e7d3998a354785d572315085e7ea176bfa298e
LucaBonifazio <luca.bonifazio13@gmail.com>
2022-10-20 15:42:02 +0200

    little fix

--- js/script.js
+++ js/script.js
@@ -7,20 +7,20 @@
 
 
 const Km = parseFloat(prompt('Benvenuto su TrenItalia.com, quanti kilometri desidera fare?'));
-console.log('I Km desiderati sono:' + Km); 
+console.log(`I Km desiderati sono:${Km}`); 
 const age = parseInt(prompt('Quanti anni ha?')); 
-console.log('La sua età è:' + age); 
+console.log(`La sua età è:${age}`); 
 const price = parseFloat(Km * 0.21).toFixed(2); 
-console.log('Il prezzo del percorso è di:' + price + '€'); 
+console.log(`Il prezzo del biglietto è di:${price}€`); 
 let sales = "";
 const newPrice = (price - sales)
 
 if (age < 18) {
     sales = ((price * 20) / 100).toFixed(2); 
-    console.log('Essendo minorenne ha diritto al 20% di sconto, pertanto il prezzo sarà di:' + newPrice + '€'); 
+    console.log(`Essendo minorenne ha diritto al 20% di sconto, pertanto il prezzo sarà di:${newPrice}€`); 
 } else if (age > 65) {
     sales = ((price * 40) / 100).toFixed(2); 
-    console.log('Essendo over 65 anni ha diritto al 40% di sconto, pertanto il prezzo sarà di:' + newPrice + '€');
+    console.log(`Essendo over 65 anni ha diritto al 40% di sconto, pertanto il prezzo sarà di:${newPrice}€`);
 }
 
 
